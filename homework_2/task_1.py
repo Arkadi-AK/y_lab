e027b67e0d286205cf5a5b6f1505bedd25154c95
Arkadi-AK <asfd@sfd.dd>
2022-07-06 09:59:15 +0300

    homework_2 modification task_1, task_2

--- homework_2/task_1.py
+++ homework_2/task_1.py
@@ -4,39 +4,37 @@ point_3 = (5, 2)  # Ул. Бейкер стрит, 221б – (5, 2)
 point_4 = (6, 6)  # Ул. Большая Садовая, 302-бис – (6, 6)
 point_5 = (8, 3)  # Вечнозелёная Аллея, 742 – (8, 3)
 
-# пример подстановки значений в формулу
-# ((point_2[0] - point_1[0]) ** 2 + (point_2[1] - point_1[1]) ** 2) ** 0.5
-
-# маршрут
+# route
 points = [(0, 2), (2, 5), (6, 6), (8, 3), (5, 2)]
 
-# начальная точка
+# starting point
 start_point = min(points)
 
 
 def calculate_points(first_point: tuple, second_point: tuple) -> float:
-    """Функция для вычисления расстояния между точками"""
+    """calculates distances between points"""
     return (
-        (second_point[0] - first_point[0]) ** 2
-        + (second_point[1] - first_point[1]) ** 2
-    ) ** 0.5
+                   (second_point[0] - first_point[0]) ** 2
+                   + (second_point[1] - first_point[1]) ** 2
+           ) ** 0.5
 
 
-def finding_short_path():
-    """Функция перебирает все точки и выводит путь"""
+def calculate_short_path(route):
+    """Iterates through all the points and outputs the path"""
     calculated_points = 0
     last_point = 0
     print(start_point, end=" -> ")
-    for current_point in range(len(points) - 1):
+    for current_point in range(len(route) - 1):
         last_point = current_point
         next_point = current_point + 1
-        current_path = calculate_points(points[current_point], points[next_point])
+        current_path = calculate_points(route[current_point], route[next_point])
         calculated_points += current_path
-        print(f"{points[next_point]}[{calculated_points}]", end=" -> ")
+        print(f"{route[next_point]}[{calculated_points}]", end=" -> ")
     finish_point = calculated_points + calculate_points(
-        points[last_point + 1], start_point
+        route[last_point + 1], start_point
     )
     print(f"{start_point}, [{finish_point}] = {finish_point}")
 
 
-finding_short_path()
+if __name__ == "__main__":
+    calculate_short_path(points)
